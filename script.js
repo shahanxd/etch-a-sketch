f3be641b3b3845a59b42a357410a3180d2c466ca
shahanxd <shahanxd@gmail.com>
2025-06-05 21:25:25 +0530

    event listener added for size input

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('Etch-a-sketch is ready!');
+document.addEventListener('DOMContentLoaded', () => {
+
+    console.log('Etch-a-sketch is ready!');
+
+    const container = document.querySelector('.container');
+    const form = document.getElementById('sizeForm');
+    const sizeInput = document.getElementById('sizeInput');
+
+    createGrid(4);
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        createGrid(sizeInput.value);
+    });
+
+    function createGrid(size) {
+        container.innerHTML = '';
+        const boxSize = 640 / size;
+
+        for (let i = 0; i < size * size; i++) {
+            const box = document.createElement('div');
+            box.classList.add('box');
+            box.style.width = `${boxSize}px`;
+            box.style.height = `${boxSize}px`;
+            box.addEventListener('mouseover', () => {
+                box.style.backgroundColor = 'black';
+            });
+            container.appendChild(box);
+        }
+    }
+});
